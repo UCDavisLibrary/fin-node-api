748f982b050c55a59cbe0acc1a0f1e81f956eadf
Justin Merz <jrmerz@gmail.com>
2018-02-27 13:45:32 -0800

    start of new collection item methods

--- tests/collections/index.js
+++ tests/collections/index.js
@@ -29,22 +29,84 @@ describe('Collection Tests', function() {
 
     assert.deepEqual(response.data, { 
       path: '/integration-test/collection/test-collection',
-      members: '/integration-test/collection/test-collection/members',
       groups: '/integration-test/collection/test-collection/groups',
       acl: '/integration-test/collection/test-collection/.acl' 
     });
   });
 
-  it('Should let you create a collection member', async function(){
-    let response = await API.collection.addMember({
-      id : 'earth-rise',
+  it('Should let you create a collection relation', async function(){
+    let response = await API.collection.createRelationContainer({
+      id : 'items',
       collectionId : 'test-collection',
-      file : path.join(__dirname, 'earthrise.jpg')
+      type : 'part'
+    });
+
+    assert.equal(response.error, null);
+    assert.equal(response.last.statusCode, 201);
+  });
+
+  it('Should let you create a collection member', async function(){
+    let response = await API.collection.addItem({
+      id : 'space',
+      collectionId : 'test-collection',
+      parentPath : 'items',
+      fsPath : path.join(__dirname, 'space')
+    });
+
+    assert.equal(response.error, null);
+    assert.equal(response.last.statusCode, 201);
+  });
+
+  it('Should let you create a media relation', async function(){
+    let response = await API.collection.createRelationContainer({
+      id : 'media',
+      membershipResource : 'items/space',
+      collectionId : 'test-collection',
+      type : 'media'
+    });
+
+    assert.equal(response.error, null);
+    assert.equal(response.last.statusCode, 201);
+  });
+
+  it('Should let you add main image to media relation', async function(){
+    let response = await API.collection.addItem({
+      id : 'cover-art',
+      collectionId : 'test-collection',
+      parentPath : 'items/space/media',
+      fsPath : path.join(__dirname, 'space'),
+      data : 'media/earthrise.jpg'
     });
 
     assert.equal(response.error, null);
     assert.equal(response.last.statusCode, 204);
   });
+
+
+  it('Should let you add images to media relation', async function(){
+    let response = await API.collection.addItem({
+      id : 'images/rings',
+      collectionId : 'test-collection',
+      parentPath : 'items/space/media',
+      fsPath : path.join(__dirname, 'space', 'media', 'images'),
+      data : 'rings.jpg',
+    });
+
+    assert.equal(response.error, null);
+    assert.equal(response.last.statusCode, 201);
+
+    response = await API.collection.addItem({
+      id : 'images/PIA21340',
+      collectionId : 'test-collection',
+      parentPath : 'items/space/media',
+      fsPath : path.join(__dirname, 'space', 'media', 'images'),
+      data : 'PIA21340.png',
+    });
+
+    assert.equal(response.error, null);
+    assert.equal(response.last.statusCode, 201);
+  });
+
 
   it('Should let you delete a collection member', async function(){
     let response = await API.collection.deleteMember({
